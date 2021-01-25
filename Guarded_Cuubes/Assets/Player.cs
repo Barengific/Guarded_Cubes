ab7b8abe317ff5629f68feb099b21aa9c4250380
Barengific <75044077+Barengific@users.noreply.github.com>
2021-01-25 23:42:27 +0000

    UI and Basics
    UI Button and Joystick Implemented into the Canvas settings. Gameobject can be controlled with it.

---
+++ Guarded_Cuubes/Assets/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public GameObject player;
+    //private PlayerController playerController;
+
+    public Button dBtn;
+    public Button uBtn;
+    public Button lBtn;
+    public Button rBtn;
+
+    void Start() {
+
+        player = GameObject.Find("Cube");
+
+        dBtn.GetComponent<Button>();
+        dBtn.onClick.AddListener(DOnClick);
+
+        uBtn.GetComponent<Button>();
+        uBtn.onClick.AddListener(UOnClick);
+
+        lBtn.GetComponent<Button>();
+        lBtn.onClick.AddListener(LOnClick);
+
+        rBtn.GetComponent<Button>();
+        rBtn.onClick.AddListener(ROnClick);
+    }
+
+    void DOnClick() {
+        Debug.Log("down");
+        player.transform.position += Vector3.right * 100.0f * Time.deltaTime;
+    }
+    void UOnClick() {
+        Debug.Log("up");
+        player.transform.position += Vector3.left * 100.0f * Time.deltaTime;
+    }
+    void LOnClick() {
+        Debug.Log("left");
+    }
+    void ROnClick() {
+        Debug.Log("right");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("w")) {
+            print("up key was pressed");
+        }
+        if (Input.GetKeyDown("s")) {
+            print("down key was pressed");
+        }
+        if (Input.GetKeyDown("a")) {
+            print("left key was pressed");
+        }
+        if (Input.GetKeyDown("d")) {
+            print("right key was pressed");
+        }
+        if (Input.GetKeyDown("space")) {
+            print("jump key was pressed");
+        }
+
+    }
+}
---
+++ Guarded_Cuubes/Assets/imports/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour {
+    public float speed;
+    public FloatingJoystick floatingJoystick;
+    public Rigidbody rb;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+
+    void Start() {
+        rb.GetComponent<Rigidbody>();
+    }
+
+
+    public void FixedUpdate() {
+        Vector3 direction = Vector3.forward * floatingJoystick.Vertical + Vector3.right * floatingJoystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+    }
+
+    void Update() {
+        float hoz = floatingJoystick.Horizontal;
+        float ver = floatingJoystick.Vertical;
+
+        Vector3 direction = new Vector3(hoz, 0, ver);
+        rb.transform.Translate(direction * 0.02f, Space.Self);
+
+        //Vector3 ldir = new Vector3(ver, hoz, 0);
+        //rb.transform.Rotate(ldir * 1f, Space.World);
+    }
+}
